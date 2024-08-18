b400caff837a3c2ba19647ad5c9a596be583aef3
Wayren <77871399@qq.com>
2024-08-18 13:08:20 +0800

    1

--- public/index.php
+++ public/index.php
@@ -1,11 +1,8 @@
 <?
 declare (strict_types = 1);
 
-// require_once __DIR__ . '/../lim';
-
 !defined('ROOT_PATH') && define('ROOT_PATH', dirname(__DIR__) . '/');
 
 require_once ROOT_PATH . 'vendor/autoload.php';
 
-// json($_SERVER);
 lim\Nginx::run();
